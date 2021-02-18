9317208f12b0a75e1a3db7f3c5675998836d2d8e
kyohei03 <kyohei03x@gmail.com>
2021-02-18 20:49:04 +0900

    マイグレーションファイル・Userのバリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :items
-  has_many :orders
 
-  validates : nickname, presence: true
-  validates : family_name, presence: true
-  validates : first_name, presence: true
-  validates : family_name_kana, presence: true
-  validates : first_name_kana, presence: true
-  validates : birthday, presence: true
+  validates :nickname, presence: true
+  validates :family_name, presence: true
+  validates :first_name, presence: true
+  validates :family_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birthday, presence: true
 end
