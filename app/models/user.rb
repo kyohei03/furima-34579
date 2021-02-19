54e1fa03770cf6e9da1444f8dddf2ed4d66fc1cb
kyohei03 <kyohei03x@gmail.com>
2021-02-19 12:50:28 +0900

    rails g devise:viewsを実施・furima素材を導入（registrations・sessions）

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,5 +21,5 @@ class User < ApplicationRecord
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'Password Include both letters and numbers'
-  end
+  
 end
