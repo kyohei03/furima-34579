3ec27b42613267eebcf12f950b9163bd38bdbd12
kyohei03 <kyohei03x@gmail.com>
2021-02-20 16:28:27 +0900

    テストコード完了

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,13 @@
 FactoryBot.define do
   factory :user do
-    name {Faker::Name.last_name}
-    email {Faker::Internet.free_email}
-    password = Faker::Internet.password(min_length: 6)
-    password {password}
+    nickname              {'test'}
+    email                 {'test@example'}
+    family_name           {'山田'}
+    first_name            {'太郎'}
+    family_name_kana      {'ヤマダ'}
+    first_name_kana       {'タロウ'}
+    birthday              {'1989-05-30'}
+    password              {'sb851097'}
     password_confirmation {password}
   end
 end
