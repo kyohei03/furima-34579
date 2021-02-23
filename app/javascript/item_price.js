b561571cb3db4a003ae61e20c5c65212e92049bc
kyohei03 <kyohei03x@gmail.com>
2021-02-23 14:15:42 +0900

    修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -8,19 +8,4 @@ window.addEventListener('load', function tax(item_price){
     const addProfitDom = document.getElementById("profit");
     addProfitDom.innerHTML = price - (price / 10)
   });
-
-
-
-  // $("#item_price").on('keyup', function(){
-  //   var price = $("#item_price").value();
-  //   if( 300 <= price && price <= 9999999) {
-  //   var add-tax-price = Math.floor(price / 10);
-  //   var profit = dd-tax-p(price - add-tax-price);
-  //   $(".arice-span").text(add-tax-price);
-  //   $(".profit-span").text(profit);
-  //   }
-  //   else {
-  //   $(".add-tax-price-span").text('');
-  //   $(".profit-span").text('');
-  //   }
   })
