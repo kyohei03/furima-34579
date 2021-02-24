0291dc167eca77e6162222c2217e178963aec7a3
kyohei03 <kyohei03x@gmail.com>
2021-02-24 15:45:29 +0900

    商品情報編集機能　実装済（仮）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create,]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -15,6 +15,22 @@ class ItemsController < ApplicationController
       redirect_to items_path
     else
       render :new
+    end
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+    unless current_user == @item.user
+      redirect_to action: :index
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id]) 
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
     end
   end
 
