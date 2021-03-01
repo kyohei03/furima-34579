c05560afb268ca26f646a7906d84c620f8e90e9a
kyohei03 <kyohei03x@gmail.com>
2021-03-01 16:47:58 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit, :create, :destroy]
   before_action :item_find, only: [:edit, :update, :show, :destroy]
   before_action :check_user, only: [:edit, :update, :destroy]
   before_action :confirm_user, only: [:edit, :update]
